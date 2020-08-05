27a691acc7570026672cf8f12a1a1ff3443aa6c1
Jan Wijffels <jwijffels@bnosac.be>
2020-08-05 18:02:09 +0200

    add cvmat_bgr

---
+++ src/base-extra.cpp
@@ -0,0 +1,9 @@
+#include "opencv_types.h"
+#include "util.hpp"
+
+// [[Rcpp::export]]
+XPtrMat cvmat_bgr(Rcpp::RawVector img, int width = 0, int height = 0){
+  std::vector<uchar> x = Rcpp::as<std::vector<uchar>>(img);
+  cv::Mat output(height, width, CV_8UC3, x.data());
+  return cvmat_xptr(output);
+}
