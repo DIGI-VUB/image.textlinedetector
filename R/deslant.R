0b8fc2013902963e26095a4cb6bec343a482df61
Jan Wijffels <jwijffels@bnosac.be>
2021-11-24 15:59:12 +0100

    add image_deslant

--- R/deslant.R
+++ R/deslant.R
@@ -1,8 +1,12 @@
 
 #' @title Deslant images by putting cursive text upright
 #' @description This algorithm sets handwritten text in images upright by removing cursive writing style. 
-#' One can use it as a preprocessing step for handwritten text recognition.
-#' @param x an object of class opencv-image with pixel values between 0 and 255
+#' One can use it as a preprocessing step for handwritten text recognition.\cr
+#' \itemize{
+#' \item{ocv_deslant expects a magick-image and does not performs grayscaling}
+#' \item{image_deslant expects a magick-image and performs grayscaling when calling the function}
+#' }
+#' @param image an object of class opencv-image (ocv_deslant) with pixel values between 0 and 255 or a magick-image (image_deslant)
 #' @param bgcolor integer value with the background color to use to fill the gaps of the sheared image that is returned. Defaults to white: 255
 #' @param lower_bound lower bound of shear values. Defaults to -1
 #' @param upper_bound upper bound of shear values. Defaults to 1
@@ -12,6 +16,7 @@
 #' \dontshow{
 #' if(require(opencv)) \{
 #' }
+#' library(magick)
 #' library(opencv)
 #' library(image.textlinedetector)
 #' path <- system.file(package = "image.textlinedetector", "extdata", "cursive.png")
@@ -20,12 +25,43 @@
 #' img
 #' up   <- ocv_deslant(img)
 #' up
+#' 
+#' img  <- image_read(path)
+#' image_deslant(img)
 #' \dontshow{
 #' \}
 #' # End of main if statement running only if the required packages are installed
 #' }
-ocv_deslant <- function(x, bgcolor = 255, lower_bound = -1, upper_bound = 1){
-  stopifnot(inherits(x, "opencv-image"))
+ocv_deslant <- function(image, bgcolor = 255, lower_bound = -1, upper_bound = 1){
+  stopifnot(inherits(image, "opencv-image"))
   bgcolor <- as.integer(bgcolor)
-  textlinedetector_deslant(x, bgcolor, lower_bound, upper_bound)
+  image.textlinedetector:::textlinedetector_deslant(image, bgcolor, lower_bound, upper_bound)
+}
+
+#' @export
+#' @rdname ocv_deslant
+image_deslant <- function(image, bgcolor = 255, lower_bound = -1, upper_bound = 1, ...){
+  stopifnot(inherits(image, "magick-image"))
+  img <- magick_to_opencv(image, ...)
+  img <- opencv::ocv_grayscale(img)
+  deslanted <- ocv_deslant(img, bgcolor, lower_bound, upper_bound)
+  opencv_to_magick(deslanted)
+}
+
+
+
+magick_to_opencv <- function(img, ...){
+  p <- tempfile()
+  on.exit({
+    if(file.exists(p)){
+      file.remove(p) 
+    }
+  })
+  magick::image_write(img, path = p, ...)
+  image <- opencv::ocv_read(p)
+  image
+}
+
+opencv_to_magick <- function(img){
+  magick::image_read(opencv::ocv_bitmap(img))
 }
